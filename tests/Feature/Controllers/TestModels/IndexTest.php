922c28b5ea034066fb84611a5b27661f48bc0f9c
Thomas van der Veen <thomas11juni@hotmail.com>
2019-10-04 09:53:32 +0200

    Base request test.

---
+++ tests/Feature/Controllers/TestModels/IndexTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace OwowAgency\LaravelResources\Tests\Feature\Controllers\TestModels;
+
+use OwowAgency\LaravelResources\Tests\TestCase;
+
+class IndexTest extends TestCase
+{
+    /** @test */
+    public function index_can_be_requested()
+    {
+        $response = $this->get('test-models');
+
+        $response->assertOk();
+    }
+}
