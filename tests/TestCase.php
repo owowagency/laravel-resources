af02cc3f7c7299735b1cea2425f909f30e51d9ef
Thomas van der Veen <thomas11juni@hotmail.com>
2019-10-04 09:49:48 +0200

    Base db testing.

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -7,7 +7,25 @@ use OwowAgency\LaravelResources\LaravelResourcesServiceProvider;
 
 class TestCase extends BaseTestCase
 {
-    protected function getPackageProviders($app)
+    /**
+     * Setup the test environment.
+     */
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->loadMigrationsFrom(
+            __DIR__ . 'support/database/migrations'
+        );
+    }
+
+    /**
+     * Get package providers.
+     *
+     * @param  \Illuminate\Foundation\Application  $app
+     * @return array
+     */
+    protected function getPackageProviders($app): array
     {
         return [
             LaravelResourcesServiceProvider::class,
---
+++ tests/support/Models/TestModel.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace OwowAgency\LaravelResources\Tests\Support\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class TestModel extends Model
+{
+    //
+}
